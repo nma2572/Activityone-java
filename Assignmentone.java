a98773cdb779d7c28f046b840921b98a1f8c7146
nma2572 <156881066+nma2572@users.noreply.github.com>
2024-10-12 21:31:43 +0400

    Add files via upload
    Looks like the
     third and one before final commit

--- Assignmentone.java
+++ Assignmentone.java
@@ -1,7 +1,18 @@
 package project;
 
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /** Natnael-Argaw(399006831)
- * Created the interfaces, the Airplane abstract class and the class for each plane - */
+ * Created the interfaces, the Airplane abstract class and the class for each plane 
+ * 
+ * Abebe-Endalemie
+ * implemented teh file handling class*/
+
 public class Assignmentone {
 
     // Abstract class Airplane
@@ -22,7 +33,7 @@ public class Assignmentone {
             this.flightduration = flightduration;
         }
 
-        public abstract void displayDetails();
+        public abstract void displayDetails();  // Metho that is gone be implemented by children
     }
 
     // Cargo interface
@@ -52,61 +63,66 @@ public class Assignmentone {
             this.cargoWeight = cargoWeight;
         }
 
-        // Implementing methods from Cargo interface
+        // using methods from cargo
         @Override
         public float getCargoWeight() {
             return cargoWeight;
         }
-        
-        
+
         @Override
         public void setCargoWeight(float weight) {
             this.cargoWeight = weight;
         }
 
-        // Implementing the abstract method from Airplane
+        // abstract method from airPlane
         @Override
         public void displayDetails() {
-            System.out.println("Flight Number: " + flightnumber + 
-                               ", Make: " + make + 
-                               ", Type: " + type + 
-                               ", Departure Time: " + departuretime + 
-                               ", Landing Time: " + landingtime + 
-                               ", Flight Duration: " + flightduration + 
+            System.out.println("Flight Number: " + flightnumber +
+                               ", Make: " + make +
+                               ", Type: " + type +
+                               ", Departure Time: " + departuretime +
+                               ", Landing Time: " + landingtime +
+                               ", Flight Duration: " + flightduration +
                                ", Cargo Weight: " + cargoWeight);
         }
-        
-        public class PrivatePlane extends Airplane implements Private {
-            private String ownername;
+    }
 
-            public PrivatePlane(String flightNumber, String make, String type, String departureTime, String landingTime, int flightDuration, String ownername) {
-                super(flightNumber, make, type, departureTime, landingTime, flightDuration);
-                this.ownername = ownername;
-            }
+    // privateplane implementing Airplane and Private
+    public class PrivatePlane extends Airplane implements Private {
+        private String ownername;
 
-            @Override
-            public String getOwnerName() {
-                return this.ownername;
-            }
+        public PrivatePlane(String flightNumber, String make, String type, String departureTime, String landingTime, double flightDuration, String ownername) {
+            super(flightNumber, make, type, departureTime, landingTime, flightDuration);
+            this.ownername = ownername;
+        }
 
-            @Override
-            public void setOwnerName(String name) {
-                this.ownername = name;
-            }
+        @Override
+        public String getOwnerName() {
+            return this.ownername;
+        }
 
-            public void displayDetails() {
-                System.out.println("Flight Number: " + flightnumber + 
-                                   ", Make: " + make + 
-                                   ", Type: " + type + 
-                                   ", Departure Time: " + departuretime + 
-                                   ", Landing Time: " + landingtime + 
-                                   ", Flight Duration: " + flightduration + 
-                                   ", Cargo Weight: " + ownername);
+        @Override
+        public void setOwnerName(String name) {
+            this.ownername = name;
+        }
 
-    }    public class Commercialplane extends Airplane implements Commercial {
+        @Override
+        public void displayDetails() {
+            System.out.println("Flight Number: " + flightnumber +
+                               ", Make: " + make +
+                               ", Type: " + type +
+                               ", Departure Time: " + departuretime +
+                               ", Landing Time: " + landingtime +
+                               ", Flight Duration: " + flightduration +
+                               ", Owner Name: " + ownername);
+        }
+    }
+
+    // commerciallane class implementing Airplane and Commercial
+    public class CommercialPlane extends Airplane implements Commercial {
         private int numberofpassengers;
 
-        public Commercialplane(String flightNumber, String make, String type, String departureTime, String landingTime, int flightDuration, int numberofpassengers) {
+        public CommercialPlane(String flightNumber, String make, String type, String departureTime, String landingTime, double flightDuration, int numberofpassengers) {
             super(flightNumber, make, type, departureTime, landingTime, flightDuration);
             this.numberofpassengers = numberofpassengers;
         }
@@ -121,17 +137,130 @@ public class Assignmentone {
             this.numberofpassengers = numberofpassengers;
         }
 
+        @Override
         public void displayDetails() {
-            System.out.println("Flight Number: " + flightnumber + 
-                               ", Make: " + make + 
-                               ", Type: " + type + 
-                               ", Departure Time: " + departuretime + 
-                               ", Landing Time: " + landingtime + 
-                               ", Flight Duration: " + flightduration + 
-                               ", Cargo Weight: " + numberofpassengers);
+            System.out.println("Flight Number: " + flightnumber +
+                               ", Make: " + make +
+                               ", Type: " + type +
+                               ", Departure Time: " + departuretime +
+                               ", Landing Time: " + landingtime +
+                               ", Flight Duration: " + flightduration +
+                               ", Number of Passengers: " + numberofpassengers);
+        }
+    }
+
+    // FileHandling class to write airplane schedule to a file and load flights
+    public class FileHandling {
+
+        
+        public void addSchedule(String filePath, Airplane[] airplanes) {
+            FileWriter writer = null;
+            try {
+                writer = new FileWriter(filePath);
+
+                
+                writer.append("flightNumber,airplaneMake,type,departureTime,landingTime,flightDuration\r\n");
+
+                
+                for (Airplane plane : airplanes) {
+                    writer.append(plane.flightnumber).append(",")
+                          .append(plane.make).append(",")
+                          .append(plane.type).append(",")
+                          .append(plane.departuretime).append(",")
+                          .append(plane.landingtime).append(",")
+                          .append(String.valueOf(plane.flightduration)).append("\r\n");
+
+                    if (plane instanceof CargoPlane) {
+                        CargoPlane cargoplane = (CargoPlane) plane;
+                        writer.append(String.valueOf(cargoplane.getCargoWeight())).append(" kilo of cargo");
+                    } else if (plane instanceof PrivatePlane) {
+                        PrivatePlane privateplane = (PrivatePlane) plane;
+                        writer.append(privateplane.getOwnerName());
+                    } else if (plane instanceof CommercialPlane) {
+                        CommercialPlane commercialplane = (CommercialPlane) plane;
+                        writer.append(String.valueOf(commercialplane.getnumberofpassengers())).append(" Passengres");
+                    }
+                }
+
+                writer.append("\r\n");
+
+                
+                writer.flush();
+                System.out.println("Flight is successfully written to file.");
+
+            } catch (IOException e) {
+                System.out.println("An error occurred  writing to the file: " + e.getMessage());
+            } finally {
+                try {
+                    if (writer != null) {
+                        writer.close();
+                    }
+                } catch (IOException ex) {
+                    System.out.println("Error closing the writer: " + ex.getMessage());
+                }
+            }
+        }
+
+        // Loading dligts from fiels
+        public List<Airplane> loadFlight(String filepath) {
+            List<Airplane> airplane = new ArrayList<>();
+
+            try (BufferedReader br = new BufferedReader(new FileReader(filepath))) {
+                String line;
+                br.readLine(); // to skip header line
+
+                while ((line = br.readLine()) != null) {
+                    String[] detail = line.split(",");   Splittting  using comma
+
+                    String flightnumber = detail[0];
+                    String make = detail[1];
+                    String type = detail[2];
+                    String departuretime = detail[3];
+                    String landingtime = detail[4];
+                    double flightduration = Double.parseDouble(detail[5]);
+
+                    if (type.equals("Cargo")) {
+                        float cargoWeight = Float.parseFloat(detail[6].replace("kilos of cargo", ""));
+                        airplane.add(new CargoPlane(flightnumber, make, type, departuretime, landingtime, flightduration, cargoWeight));
+                    } else if (type.equals("Private")) {
+                        String ownerName = detail[6];
+                        airplane.add(new PrivatePlane(flightnumber, make, type, departuretime, landingtime, flightduration, ownerName));
+                    } else if (type.equals("Commercial")) {
+                        int numberOfPassengers = Integer.parseInt(detail[6].replace(" Passengers", ""));
+                        airplane.add(new CommercialPlane(flightnumber, make, type, departuretime, landingtime, flightduration, numberOfPassengers));
+                    } else {
+                        System.out.println("Unknown airplane type: " + type);
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Error while reading the file: " + e.getMessage());
+            }
+            return airplane;
+        }
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        Assignmentone system = new Assignmentone();
+        FileHandling filehandler = system.new FileHandling(); 
+       
+        List<Airplane> departureFlights = filehandler.loadFlight("C:\\Users\\nhatt\\OneDrive\\Documents\\python\\DataStructure\\depratureFlights.csv");
+        List<Airplane> landingFlights = filehandler.loadFlight("C:\\Users\\nhatt\\OneDrive\\Documents\\python\\DataStructure\\landingFlights.csv");
+
+        
+        for (Airplane plane : landingFlights) {
+            plane.displayDetails();
         }
         
+        for (Airplane plane : departureFlights) {
+            plane.displayDetails();
         }
+
+        
     }
-        }
+
     }
+
+        
+
+
